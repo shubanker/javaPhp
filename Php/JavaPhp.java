83c9c302339b2d1baa655606f9e2fe6985e18604
Subhanker Chourasia <subhankerchourasia@gmail.com>
2015-03-01 13:17:49 +0530

    Saperated Php And javaPhp in their respective file Names

---
+++ Php/JavaPhp.java
@@ -0,0 +1,5 @@
+package Php;
+
+public class JavaPhp extends ExtraFunctions{
+
+}
--- Php/Php.java
+++ Php/Php.java
@@ -6,9 +6,6 @@ package Php;
  * Either Extend Your Class With JavaPhp or  PHP And All The functions will be avialble to you
  * Alternatively You Can Put Class File in Same directory And use functions with Class Name Such As Php.echo("Hello");
  */
-class JavaPhp extends ExtraFunctions {
-
-}
 public class Php extends ExtraFunctions {
 
 }

e760398c41bad8cf36e7f7aad7ac2ae3b240f4b6
Shubanker Chourasia <subhankerchourasia@gmail.com>
2015-05-23 18:02:34 +0530

    Added Demos

--- Demos/Average.java
+++ Demos/Average.java
@@ -3,9 +3,7 @@ import Php.Php;
 public class Average {
 
 	public static void main(String[] args) {
-		int sum,n;
-		sum=0;
-		n=0;
+		int sum=0,n=0;
 		while(true){
 			try {
 				sum+=Php.get_input_int("Enter the number or to exit Enter any character.");
@@ -15,7 +13,6 @@ public class Average {
 			}
 		}
 		Php.echo("Average = ");
-		Php.echo(n==0?0:(sum/n));
+		Php.echo(n==0?0:((float)sum/n));
 	}
-
 }
---
+++ Demos/Palindrom.java
@@ -0,0 +1,8 @@
+package Demos;
+import Php.Php;
+public class Palindrom {
+	public static void main(String[] args) {
+		int n=Php.get_input_int("Enter a number");
+//		Php.echo("The number is "+()+"Palindrom");
+	}
+}

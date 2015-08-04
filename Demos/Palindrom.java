753e52521ed4fdb3f3ff69a169c0cf9af1cf5669
Shubanker Chourasia <subhankerchourasia@gmail.com>
2015-08-04 16:41:21 +0530

    Fixed Palindrom demo

--- Demos/Palindrom.java
+++ Demos/Palindrom.java
@@ -3,6 +3,6 @@ import Php.Php;
 public class Palindrom {
 	public static void main(String[] args) {
 		String s=Php.getInput("Enter a String");
-		Php.echo("The String is "+(s.compareTo(Php.reverse(s))==0?"":"not ")+"Palindrom");
+		Php.echo("The String is "+(s.compareTo(Php.strrev(s))==0?"":"not ")+"Palindrom");
 	}
 }
